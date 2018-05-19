fd667cabbb63c6550804244eeaa6113e2062dbcd
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2018-05-19 19:43:09 +0200

    Event class creation

---
+++ Sine/Assets/Sine/Event.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sine
+{
+    [CreateAssetMenu(menuName = "Sine/Event", fileName = "New Event")]
+    public class Event : ScriptableObject
+    {
+        public List<Track> tracks; 
+    }
+}
+
