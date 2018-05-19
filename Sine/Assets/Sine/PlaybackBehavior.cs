9aa0ff7389d3fcdb1329eb7ae6927c1ec0fbfa9b
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2018-05-19 19:43:34 +0200

    Playback Behavior enum creation

---
+++ Sine/Assets/Sine/PlaybackBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sine
+{
+    public enum PlaybackBehavior
+    {
+        OneShot,
+        Loop,
+        Granular
+    }
+}
+
