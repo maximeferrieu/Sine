d847de2562040e2b1f319610e23625e1c456cad1
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2018-05-19 19:44:20 +0200

    Range struct creation

---
+++ Sine/Assets/Sine/Range.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Sine
+{
+    [Serializable]
+    public struct Range
+    {
+        public Range(float minValue, float maxValue)
+        {
+            min = minValue;
+            max = maxValue;
+        }
+
+        public float min;
+        public float max;
+    }
+}
+
