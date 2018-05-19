59a2ac5cc8d799300760b3383b2ea17e45cb0bf2
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2018-05-19 19:44:06 +0200

    Track class creation

---
+++ Sine/Assets/Sine/Track.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+namespace Sine
+{
+    [Serializable]
+    public class Track
+    {
+        public string name;
+
+        public PlaybackBehavior behavior;
+
+        public AudioMixerGroup output;
+
+        public Range initialDelayRange = new Range(0f, 0f);
+
+        public Range volumeRange = new Range(0f,1f);
+
+        public Range pitchRange = new Range(0.01f, 3f);
+
+        public List<AudioClip> clips = new List<AudioClip>();
+    }
+}
+
